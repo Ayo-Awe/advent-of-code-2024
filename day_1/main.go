44647bdc54f53acc8e956bbe261e6de1dfb053b9
Awe Ayomidipupo <pupoawe@gmail.com>
2024-12-06 00:19:50 +0100

    solve day_1 part_2

--- day_1/main.go
+++ day_1/main.go
@@ -10,10 +10,15 @@ import (
 	"strings"
 )
 
-func main() {
-	bytes, err := os.ReadFile("./input_1.txt")
+type Input struct {
+	left  []int
+	right []int
+}
+
+func readAndParseInput(filename string) (*Input, error) {
+	bytes, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	left := []int{}
@@ -27,12 +32,12 @@ func main() {
 
 		leftInt, err := strconv.Atoi(leftAndRight[0])
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		rightInt, err := strconv.Atoi(leftAndRight[1])
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		left = append(left, leftInt)
@@ -42,12 +47,50 @@ func main() {
 	slices.Sort(left)
 	slices.Sort(right)
 
-	sum := 0
-	for i := range len(left) {
-		sum += diff(left[i], right[i])
+	return &Input{left: left, right: right}, nil
+}
+
+func main() {
+	input, err := readAndParseInput("input_1.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(sum)
+	solutionOne := partOne(input)
+	fmt.Println("solution one:", solutionOne)
+
+	solutionTwo := partTwo(input)
+	fmt.Println("solution two:", solutionTwo)
+}
+
+func partOne(input *Input) int {
+	sum := 0
+	for i := range len(input.left) {
+		sum += diff(input.left[i], input.right[i])
+	}
+
+	return sum
+}
+
+func partTwo(input *Input) int {
+	frequencyTable := map[int]int{}
+	for _, val := range input.left {
+		frequencyTable[val] = 0
+	}
+
+	for _, val := range input.right {
+		_, exists := frequencyTable[val]
+		if exists {
+			frequencyTable[val]++
+		}
+	}
+
+	sum := 0
+	for _, val := range input.left {
+		sum += val * frequencyTable[val]
+	}
+
+	return sum
 }
 
 func diff(left, right int) int {
