31b3bf1caa261ecefc82cfaa190ebc4b836c3e75
Awe Ayomidipupo <pupoawe@gmail.com>
2024-12-11 11:11:25 +0100

    solve day_2 part_2

--- day_7/main.go
+++ day_7/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/ayo-awe/advent-of-code-2024/aoc"
 )
@@ -43,6 +45,7 @@ func main() {
 	}
 
 	fmt.Println("solution to part one: ", partOne(equations))
+	fmt.Println("solution to part two: ", partTwo(equations))
 }
 
 func partOne(eqns []Equation) int {
@@ -78,4 +81,58 @@ func evalOne(target int, operands []int) bool {
 
 	// eval + operator
 	return multiplySuccess || evalOne(target-operand, operands[:lastIndex])
+}
+
+func partTwo(eqns []Equation) int {
+	sum := 0
+	for _, eqn := range eqns {
+		if evalTwo(eqn.Target, eqn.Operands) {
+			sum += eqn.Target
+		}
+	}
+	return sum
+}
+
+func evalTwo(target int, operands []int) bool {
+	lastIndex := len(operands) - 1
+
+	// no elements left
+	if lastIndex < 0 {
+		return false
+	}
+
+	if target < 0 {
+		return false
+	}
+
+	operand := operands[lastIndex]
+
+	// only one element left
+	if lastIndex == 0 {
+		return operand == target
+	}
+
+	// eval * operator only if target is divisible by operand
+	multiplySuccess := false
+	if target%operand == 0 {
+		multiplySuccess = evalTwo(target/operand, operands[:lastIndex])
+	}
+
+	// eval || operator only if target ends with operand
+	concatSuccess := false
+	if endsWithInt(target, operand) {
+		newTarget := (target - operand) / int(math.Pow10(numberOfDigits(operand)))
+		concatSuccess = evalTwo(newTarget, operands[:lastIndex])
+	}
+
+	// eval + operator
+	return multiplySuccess || concatSuccess || evalTwo(target-operand, operands[:lastIndex])
+}
+
+func endsWithInt(target, val int) bool {
+	return strings.HasSuffix(strconv.Itoa(target), strconv.Itoa(val))
+}
+
+func numberOfDigits(num int) int {
+	return int(math.Floor(math.Log10(float64(num)))) + 1
 }
