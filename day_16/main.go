69dfaa04557ea78aae7927b47a2bc479e6b2926d
Awe Ayomidipupo <pupoawe@gmail.com>
2025-01-25 13:33:18 +0100

    solve day_16 part two

--- day_16/main.go
+++ day_16/main.go
@@ -8,11 +8,10 @@ import (
 	"github.com/ayo-awe/advent-of-code-2024/aoc"
 )
 
-type pos [3]int
+type pos [2]int
 
-func (p pos) x() int         { return p[0] }
-func (p pos) y() int         { return p[1] }
-func (p pos) dir() direction { return direction(p[2]) }
+func (p pos) x() int { return p[0] }
+func (p pos) y() int { return p[1] }
 
 type direction int
 
@@ -61,59 +60,147 @@ func main() {
 	}
 
 	grid := parseInput(input)
-	fmt.Println(PartOne(grid))
-}
-
-type state struct {
-	pos
-	score int
+	fmt.Println("solution to part one: ", PartOne(grid))
+	fmt.Println("solution to part two: ", PartTwo(grid))
 }
 
 func PartOne(grid [][]rune) int {
-	start := pos{1, len(grid) - 2, int(East)}
-	seen := make(map[pos]int)
-	queue := []state{{start, 0}}
+	type state struct {
+		pos
+		score int
+		dir   direction
+	}
+
+	start := pos{1, len(grid) - 2}
+	seen := make(map[string]int)
+	queue := []state{{start, 0, East}}
 	minScore := math.MaxInt
 
 	for len(queue) > 0 {
-		curr := queue[0]
+		currState := queue[0]
 		queue = queue[1:]
 
-		if curr.score > minScore {
+		if currState.score > minScore {
 			continue
 		}
 
-		if score, ok := seen[curr.pos]; ok && score < curr.score {
+		// we differentiate states only by their positions and directions. The score is ignored
+		stateKey := newStateKey(currState.pos, currState.dir)
+		if prevScore, ok := seen[stateKey]; ok && prevScore < currState.score {
 			continue
 		}
 
-		if grid[curr.y()][curr.x()] == 'E' {
-			minScore = curr.score
+		if grid[currState.y()][currState.x()] == 'E' {
+			minScore = currState.score
 			continue
 		}
 
 		// forward
-		delta := curr.pos.dir().delta()
-		if grid[curr.y()+delta[1]][curr.x()+delta[0]] != '#' {
-			newPos := pos{curr.x() + delta[0], curr.y() + delta[1], int(curr.dir())}
-			queue = append(queue, state{newPos, curr.score + 1})
+		delta := currState.dir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x() + delta[0], currState.y() + delta[1]}
+			queue = append(queue, state{newPos, currState.score + 1, currState.dir})
 		}
 
 		// only turn clockwise if there's an open square to your right
-		delta = curr.dir().clockwise().delta()
-		if grid[curr.y()+delta[1]][curr.x()+delta[0]] != '#' {
-			newPos := pos{curr.x(), curr.y(), int(curr.dir().clockwise())}
-			queue = append(queue, state{newPos, curr.score + 1000})
+		newDir := currState.dir.clockwise()
+		delta = newDir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x(), currState.y()}
+			queue = append(queue, state{newPos, currState.score + 1000, newDir})
 		}
 
-		delta = curr.dir().anitClockwise().delta()
-		if grid[curr.y()+delta[1]][curr.x()+delta[0]] != '#' {
-			newPos := pos{curr.x(), curr.y(), int(curr.dir().anitClockwise())}
-			queue = append(queue, state{newPos, curr.score + 1000})
+		// only turn anit-clockwise if there's an open square to your right
+		newDir = currState.dir.anitClockwise()
+		delta = newDir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x(), currState.y()}
+			queue = append(queue, state{newPos, currState.score + 1000, newDir})
 		}
 
-		seen[curr.pos] = curr.score
+		seen[stateKey] = currState.score
 	}
 
 	return minScore
+}
+
+func PartTwo(grid [][]rune) int {
+	type state struct {
+		pos
+		score int
+		dir   direction
+		path  []pos
+	}
+
+	var minScoreTiles []pos
+	start := pos{1, len(grid) - 2}
+	seen := make(map[string]int)
+	queue := []state{{start, 0, East, nil}}
+	minScore := math.MaxInt
+
+	for len(queue) > 0 {
+		currState := queue[0]
+		queue = queue[1:]
+
+		if currState.score > minScore {
+			continue
+		}
+
+		// we differentiate states only by their positions and directions. The score and path are ignored
+		stateKey := newStateKey(currState.pos, currState.dir)
+		if score, ok := seen[stateKey]; ok && score < currState.score {
+			continue
+		}
+
+		if grid[currState.y()][currState.x()] == 'E' {
+			// reset the seen min score tiles
+			if currState.score < minScore {
+				minScoreTiles = nil
+			}
+
+			minScoreTiles = append(minScoreTiles, currState.path...)
+			minScore = currState.score
+			continue
+		}
+
+		// forward
+		delta := currState.dir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x() + delta[0], currState.y() + delta[1]}
+			path := append([]pos{currState.pos}, currState.path...)
+			queue = append(queue, state{newPos, currState.score + 1, currState.dir, path})
+		}
+
+		// only explore clockwise if there's an open square to your right
+		newDir := currState.dir.clockwise()
+		delta = newDir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x(), currState.y()}
+			path := append([]pos{currState.pos}, currState.path...)
+			queue = append(queue, state{newPos, currState.score + 1000, newDir, path})
+		}
+
+		// only explore anti-clockwise if there's an open square to your left
+		newDir = currState.dir.anitClockwise()
+		delta = newDir.delta()
+		if grid[currState.y()+delta[1]][currState.x()+delta[0]] != '#' {
+			newPos := pos{currState.x(), currState.y()}
+			path := append([]pos{currState.pos}, currState.path...)
+			queue = append(queue, state{newPos, currState.score + 1000, newDir, path})
+		}
+
+		seen[stateKey] = currState.score
+	}
+
+	uniqueTiles := make(map[[2]int]struct{})
+	for _, tile := range minScoreTiles {
+		uniqueTiles[tile] = struct{}{}
+	}
+
+	// we add 1 to account for the end tile
+	return len(uniqueTiles) + 1
+}
+
+func newStateKey(p pos, d direction) string {
+	return fmt.Sprintf("%d:%d:%d", p.x(), p.y(), d)
 }
