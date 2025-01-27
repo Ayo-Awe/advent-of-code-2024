b35ab0b9a0b01027e94ea43ace1e485f4316ca0c
Awe Ayomidipupo <pupoawe@gmail.com>
2025-01-27 12:45:17 +0100

    solve day 18 part two

--- day_18/main.go
+++ day_18/main.go
@@ -51,7 +51,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(PartOne(bytePositions))
+	fmt.Println("solution to part one: ", PartOne(bytePositions))
+	fmt.Println("solution to part two: ", PartTwo(bytePositions))
 }
 
 func PartOne(bytePositions [][2]int) int {
@@ -99,4 +100,62 @@ func PartOne(bytePositions [][2]int) int {
 	}
 
 	return minSteps
+}
+
+func PartTwo(bytePositions [][2]int) string {
+	corrupted := make(map[[2]int]struct{})
+	n := 1 << 10 // 1024
+	gridSize := 71
+	end := [2]int{gridSize - 1, gridSize - 1}
+
+	for i := range n {
+		bytePos := bytePositions[i]
+		corrupted[bytePos] = struct{}{}
+	}
+
+	var blockingByte [2]int
+	for ; n < len(bytePositions); n++ {
+		seen := make(map[[2]int]struct{})
+		queue := [][3]int{{}}
+		bytePos := bytePositions[n]
+		corrupted[bytePos] = struct{}{}
+
+		var minSteps int
+		for len(queue) > 0 {
+			curr := queue[0]
+			pos := [2]int{curr[X], curr[Y]}
+			queue = queue[1:]
+
+			if _, ok := seen[pos]; ok {
+				continue
+			}
+
+			if _, ok := corrupted[pos]; ok {
+				continue
+			}
+
+			if pos[X] < 0 || pos[X] >= gridSize || pos[Y] < 0 || pos[Y] >= gridSize {
+				continue
+			}
+
+			if pos == end {
+				minSteps = curr[Steps]
+				break
+			}
+
+			for _, dir := range directions {
+				queue = append(queue, [3]int{curr[X] + dir[X], curr[Y] + dir[Y], curr[Steps] + 1})
+			}
+
+			seen[pos] = struct{}{}
+		}
+
+		// we never reached the end square
+		if minSteps == 0 {
+			blockingByte = bytePos
+			break
+		}
+	}
+
+	return fmt.Sprintf("%d,%d", blockingByte[X], blockingByte[Y])
 }
