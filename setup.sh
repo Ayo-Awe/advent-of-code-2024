d1bba6aa8cf208847bdad58772913d41de74ad56
Awe Ayomidipupo <pupoawe@gmail.com>
2025-01-29 11:26:29 +0100

    udpate setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+# Original script credits: 0xdf and sventec (GitHub)
+
+# Usage:
+# Set $AOC_SESSION to AOC session cookie: export AOC_SESSION=<cookie> or via a .env file
+# Then: source setup.sh <day#>
+#  e.g. source setup.sh 3
+# Optionally, include the year for completing previous year's challenges:
+#       source setup.sh <day#> <year>
+#  e.g. source setup.sh 3 2022
+  
 day=$1
+main_file_template=$(echo -n "package main
+
+import \"fmt\"
+
+func main() {
+
+    fmt.Println(\"solution to part one: \", PartOne())
+    fmt.Println(\"solution to part two: \", PartTwo())
+}
+
+func PartOne() int {
+    return 0
+}
+
+func PartTwo() int {
+    return 0
+}")
 
 if [ -z "$day" ]; then
-    echo "Usage: $0 <day>"
+    echo "Usage: $0 <day> [year]"
     exit 1
 fi
 
@@ -12,7 +39,7 @@ if [[ ! "$day" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-folder="day_$day"
+folder=$(printf "day_%02d" "$day")
 main_file="$folder/main.go"
 input_file="$folder/input.txt"
 
@@ -20,9 +47,25 @@ if [ ! -e "$folder" ]; then
     mkdir "$folder"
 fi
 
+
 if [ ! -e "$main_file" ]; then
-    echo -e "package main\n\nfunc main() {\n\n}" > "$main_file"
+    echo -n "$main_file_template" > "$main_file"
 fi
 
-# touch does nothing if the file already exists
-touch $input_file
+# load .env if it exists
+if [ -e ".env" ]; then
+    set -a
+    source .env
+    set +a
+fi
+
+# fetch input for current day
+if [ -z "$AOC_SESSION" ]; then
+  echo "\$AOC_SESSION isn't set. Can't fetch your input. Quitting"
+  return
+fi
+
+year=${2:-$(date +%Y)}
+url_base="https://adventofcode.com/${year}/day/${day}"
+
+curl -s "${url_base}/input" --cookie "session=${AOC_SESSION}" -o $input_file
